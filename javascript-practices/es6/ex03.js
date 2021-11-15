a318b2f3c91b1c1e1aa5f56111f836af2e631f6e
changmok <hong1071@nate.com>
2021-11-15 15:26:47 +0900

    위치 변경

--- javascript-practices/es6/ex03.js
+++ javascript-practices/es6/ex03.js
@@ -1,20 +1,17 @@
 /**
- * 
- *  Template Literal
- * 
+ * Template Literal
  */
+
 
 let no = 10;
 let name = '둘리';
 let email = 'dooly@gmail.com';
 
 // ex1
-console.log('no: ' + no + ', name: ' + name + ', email: ' + email);
-console.log('no:${no}, name:${name}, email:${email}');
+console.log('no:' + no + ", name:" + name + ", email:" + email);
+console.log(`no:${no}, name:${name}, email:${email}`);
 
 // ex2: 다중행
-console.log('
-    no:${no}
-    name:${name}
-    email:${email}
-');
+console.log(`no:${no}
+name:${name}
+email:${email}`);
--- javascript-practices/es6/ex04.js
+++ javascript-practices/es6/ex04.js
@@ -10,12 +10,3 @@ const print = function(strs, end='\n'){
 
 print(['always', 'with', 'me']);
 print(['always', 'with', 'me'], ' ');
-
-const power = function(x){
-    return x * x;
-}
-
-let numbers = [1, 2, 3, 4, 5];
-numbers.forEach(function(n){
-    process.stdout.write('${n}:${power(n)}');
-})
