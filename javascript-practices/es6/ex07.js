3aeff1449f0065c2c38398e36357d2f2b974566b
changmok <hong1071@nate.com>
2021-11-19 14:18:56 +0900

    rect

---
+++ javascript-practices/es6/ex07.js
@@ -0,0 +1,22 @@
+/*
+
+    class Rect
+
+*/
+
+class Rect {
+    constructor(w, h){
+        this.w = w;
+        this.h = h;
+    }
+    draw(){
+        console.log(`Rect(w=${this.w}, h=${this.h})`);
+    }
+}
+
+// test
+const rect1 = new Rect(10, 20);
+const rect2 = new Rect(100, 200);
+
+rect1.draw();
+rect2.draw();
