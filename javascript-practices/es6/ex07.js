34a35ff6f08305ba9bdac70988856b74ba63fe68
changmok <hong1071@nate.com>
2021-11-19 15:19:45 +0900

    JS

--- javascript-practices/es6/ex07.js
+++ javascript-practices/es6/ex07.js
@@ -20,3 +20,61 @@ const rect2 = new Rect(100, 200);
 
 rect1.draw();
 rect2.draw();
+
+
+/*
+
+    cf. 생성자 함수(prototype)
+
+*/
+const Circle = function(x, y, r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+}
+
+Circle.prototype.draw = function(){
+    console.log(`Circle(x=${this.x}, y=${this.y}, r=${this.r})을 그렸습니다`);
+}
+
+const circle1 = new Circle(10,20, 5);
+const circle2 = new Circle(100,200, 5);
+
+circle1.draw();
+circle2.draw();
+
+/*
+
+    extends
+
+*/
+class Shape {
+    constructor(bgColor, lnColor){
+        this.bgColor = bgColor;
+        this.lnColor = lnColor;
+    }
+
+    draw(){
+        console.log('그릴 수 없습니다');
+    }
+
+    area(){
+        console.log('구할 수 없습니다');
+    }
+}
+
+class Triangle extends Shape{
+    constructor(w, h){
+        super('red', 'black');
+        this.w = w;
+        this.h = h;
+    }
+    // @Override
+    draw(){
+        console.log(`Triangle(w=${this.w},h=${this.h},bgColor=${this.bgColor},lnColor=${this.lnColor})를 그렸습니다`)
+    }
+}
+
+const tri1 = new Triangle(10, 20);
+tri1.draw();
+tri1.area();
